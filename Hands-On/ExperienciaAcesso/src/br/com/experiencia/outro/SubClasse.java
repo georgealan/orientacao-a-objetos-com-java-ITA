a50f4c1dd5f8b7508be5ccbc09137b61cd660eb1
George Alan <georgealanrufo@gmail.com>
2019-12-09 13:53:24 -0300

    Hands-on de fixação de conceitos de modificadores de acesso

---
+++ Hands-On/ExperienciaAcesso/src/br/com/experiencia/outro/SubClasse.java
@@ -0,0 +1,13 @@
+package br.com.experiencia.outro;
+
+import br.com.experiencia.beans.Referencia;
+
+public class SubClasse extends Referencia {
+
+    public void testeAcesso(){
+        //modificadorDefault = 0;
+        modificadorPublico = 0;
+        modificadorProtegido = 0;
+        //modificadorPrivado = 0;
+    }
+}
